7f4b4cdfb022480efa580c72bde566f00ca74fba
hansol <thf0128@gmail.com>
2023-12-08 10:54:36 +0900

    #4. QueryDSL의 다양한 문법들(동적 SQL 활용법)

--- src/main/java/com/example/study/entity/Member.java
+++ src/main/java/com/example/study/entity/Member.java
@@ -32,11 +32,3 @@ public class Member {
 
 
 
-
-
-
-
-
-
-
-
--- src/main/java/com/example/study/entity/Team.java
+++ src/main/java/com/example/study/entity/Team.java
@@ -22,6 +22,8 @@ public class Team {
     private String name;
 
     @OneToMany(mappedBy = "team")
+    @Builder.Default
     private List<Member> members = new ArrayList<>();
+
 
 }
